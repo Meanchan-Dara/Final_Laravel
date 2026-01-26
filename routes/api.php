1518a8de0e7f383b3df2c2ba0a13e8aafca3f87f
Meanchan-Dara <meanchandara1@gmail.com>
2026-01-26 10:03:00 +0700

    Update Code

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
     //    AuthController
     Route::post('/logout', [AuthController::class, 'logout']);
     // ProdjuctController
